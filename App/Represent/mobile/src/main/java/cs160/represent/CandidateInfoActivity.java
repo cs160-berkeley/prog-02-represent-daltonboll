1ea1f5690b89a022f33f0ce58e7fe8bcff1a7082
Dalton Boll <daltonboll@gmail.com>
2016-03-07 08:53:45 -0800

    Add CandidateInfo Activity

---
+++ App/Represent/mobile/src/main/java/cs160/represent/CandidateInfoActivity.java
@@ -0,0 +1,13 @@
+package cs160.represent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CandidateInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidate_info);
+    }
+}
